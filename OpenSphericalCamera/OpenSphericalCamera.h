26da1bad0c763652d7de8fc0751c690aecab5690
Tatsuhiko Arai <tatsu@tatsu.com>
2016-06-03 10:50:49 +0900

    Initial commit

---
+++ OpenSphericalCamera/OpenSphericalCamera.h
@@ -0,0 +1,19 @@
+//
+//  OpenSphericalCamera.h
+//  OpenSphericalCamera
+//
+//  Created by Tatsuhiko Arai on 6/3/16.
+//  Copyright © 2016 Tatsuhiko Arai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenSphericalCamera.
+FOUNDATION_EXPORT double OpenSphericalCameraVersionNumber;
+
+//! Project version string for OpenSphericalCamera.
+FOUNDATION_EXPORT const unsigned char OpenSphericalCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenSphericalCamera/PublicHeader.h>
+
+
